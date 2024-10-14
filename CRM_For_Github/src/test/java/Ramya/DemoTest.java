039e38897fa90cb53a4a5dd377ae6a857d03e9a0
ACER <ACER@DESKTOP-VHBPG3D>
2024-10-14 18:13:46 +0530

    Ramya's Commit

--- CRM_For_Github/src/test/java/Ramya/DemoTest.java
+++ CRM_For_Github/src/test/java/Ramya/DemoTest.java
@@ -1,6 +1,7 @@
 package Ramya;
 
 public class DemoTest {
+	@Test
 	public void DemoScriptFroGithub(){
 		System.out.println("Start writing Demo Test scripts");
 		System.out.println("Commit aftere conflict");
